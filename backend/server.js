e29dfb749ba9c0a38a4fede838fa11493b5c1f4f
spenceradolph <c19spencer.adolph@usafa.edu>
2019-02-02 13:21:19 -0700

    fixing old fragments

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,7 @@ db.connect((err) => {
 
 
 app.get('/', (req, res) => {
+    //TODO: if the session gameId and teamId are not null, change to logged out inside the database? (probably woudldn't affect much tho)
     req.session.gameId = null;
     req.session.teamId = null;
     res.sendFile(__dirname + '/index.html');
@@ -41,7 +42,7 @@ app.get('/game.html', (req, res) => {
     if (!req.session.gameId || !req.session.teamId) {
         res.redirect('/index.html?err=NotLoggedIn');
     } else {
-        res.sendFile(__dirname + '/game.html');
+        res.redirect('localhost:3000');
     }
 });
 
@@ -53,7 +54,8 @@ app.get('/admin.html', (req, res) => {
     }
 });
 
-//TODO: change this to a socket check
+//Could change this to a socket check from socket connection on the index
+//TODO change this call back hell into promises and stuff
 app.get('/gameLoginVerify', (req, res) => {
     if(!req.query.gameSection || !req.query.gameInstructor || !req.query.gameTeam) {
         res.redirect('/index.html?err=BadParameters');  // didn't hit the backend with right query parameters
@@ -71,45 +73,19 @@ app.get('/gameLoginVerify', (req, res) => {
                     req.session.teamId = req.query.gameTeam;
                     res.redirect('game.html');
                 } else {
-                    //(could make this cleaner with better select statement, but exception for spec (do we need spec?))
-                    let teamLoggedIn;
-                    switch(parseInt(req.query.gameTeam)) {
-                        case 0:
-                            teamLoggedIn = results[0].gameTeam0;
-                            break;
-                        case 1:
-                            teamLoggedIn = results[0].gameTeam1;
-                            break;
-                        case 2:
-                            teamLoggedIn = results[0].gameTeam2;
-                            break;
-                        case 3:
-                            teamLoggedIn = results[0].gameTeam3;
-                            break;
-                        case 4:
-                            teamLoggedIn = results[0].gameTeam4;
-                            break;
-                        case 5:
-                            teamLoggedIn = results[0].gameTeam5;
-                            break;
-                        case 6:
-                            teamLoggedIn = results[0].gameTeam6;
-                            break;
-                        case 7:
-                            teamLoggedIn = results[0].gameTeam7;
-                            break;
-                    }
+                    const allReturnTeamValues = [results[0].gameTeam0, results[0].gameTeam1, results[0].gameTeam2, results[0].gameTeam3, results[0].gameTeam4, results[0].gameTeam5, results[0].gameTeam6, results[0].gameTeam7];
+                    let teamLoggedIn = allReturnTeamValues[req.query.gameTeam];                        
                     if (parseInt(teamLoggedIn) === 1) {
                         res.redirect('/index.html?err=AlreadyLoggedIn');
                     } else {
-                        req.session.gameId = results[0].gameId;
-                        req.session.teamId = req.query.gameTeam;
+                        req.session.gameId = parseInt(results[0].gameId);
+                        req.session.teamId = parseInt(req.query.gameTeam);
                         sql = `UPDATE games SET ?? = 1 WHERE gameId = ?`;
                         let inserts = ['gameTeam' + req.query.gameTeam, results[0].gameId];
                         sql = mysql.format(sql, inserts);
                         db.query(sql, (err, results) => {
                             if(err) throw err;
-                            res.redirect('game.html');
+                            res.redirect('http://localhost:3000');
                         });
                     }
                 }
@@ -141,9 +117,25 @@ app.get('/adminLoginVerify', (req, res) => {
 
 
 io.sockets.on('connection', (socket) => {
-    console.log("socket connected");
-
-    console.log(socket.handshake.session.someSessionVariable);
+    socket.on('getGamePositions', (callback) => {
+        const gameId = socket.handshake.session.gameId;
+        const teamId = socket.handshake.session.teamId;
+        let totalResults = {};
+        totalResults.positions = [];
+        for (let x = 0; x < 727; x++) {
+            totalResults.positions[x] = [];
+        }
+        let sql = 'SELECT pieceId, pieceFuel, pieceMoves, pieceUnitId, pieceTeamId, piecePositionId, pieceContainerId FROM pieces WHERE pieceGameId = ? AND (pieceVisible = 1 OR pieceTeamId = ?) ORDER BY piecePositionId';
+        let inserts = [gameId, teamId];
+        sql = mysql.format(sql, inserts);
+        db.query(sql, (err, results) => {
+            if(err) throw err;
+            results.map(result => {
+                totalResults.positions[result.piecePositionId].push(result);
+            });
+            callback(totalResults);
+        });
+    });
 });
 
 
